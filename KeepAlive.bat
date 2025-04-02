62a52ef13549d2472346d1c01717ec67ada49496
Maciej <39693718+bonczyk@users.noreply.github.com>
2025-04-02 12:12:29 +0200

    Update KeepAlive.bat

--- KeepAlive.bat
+++ KeepAlive.bat
@@ -1,4 +1,3 @@
-ppowershell -command "clear;$host.UI.RawUI.WindowSize=New
-Object System.Management.Automation.Host.size(35,2);
-$wsh-New-Object -Com WScript.Shell; while (1) [Swsh.Sendkeys
-("+[F15]");Get-date; Sleep -s 59]"
+powershell -command "clear;$host.UI.RawUI.WindowSize=New-Object System.Management.Automation.Host.size(35,2);
+$wsh-New-Object -Com WScript.Shell; while (1) {Swsh.Sendkeys
+("+[F15]");Get-date; Sleep -s 59}"
